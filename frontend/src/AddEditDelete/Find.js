7d2e9922e5f44b0fe648fccb7b7e571651ce720c
mahmutsel <mahmutsel29@gmail.com>
2020-07-13 09:56:34 +0100

    some css changes

--- frontend/src/AddEditDelete/Find.js
+++ frontend/src/AddEditDelete/Find.js
@@ -2,35 +2,33 @@ import React from "react";
 import style from "./find.module.css";
 const Find = () => {
   return (
-    <div className="search">
-      <div className="page-header">
-        <h4 className="text-left">Week Finder</h4>
+    <div className={style.find}>
+      <div className={style.header}>
+        <h4 className={style.headertext}>Week Finder</h4>
       </div>
       <div className="row search-wrapper">
         <div className="col">
-          <form className="form-group search-box">
-            <label htmlFor="customerName">Week Number</label>
-            <div className="search-row">
+          <form className={style.form}>
+            <label>
+              Week Number
               <input
                 type="text"
                 id="customerName"
                 className="form-control"
                 placeholder="Customer name"
-                value=""
               />
-            </div>
-            <label htmlFor="customerName">Subject</label>
-            <div className="search-row">
+            </label>
+            <label>
+              Subject
               <input
                 type="text"
                 id="customerName"
                 className="form-control"
                 placeholder="Customer name"
-                value=""
               />
-            </div>
-            <div>
-              <button className="btn btn-primary">Find</button>
+            </label>
+            <div className={style.button}>
+              <button>Find</button>
             </div>
           </form>
         </div>
