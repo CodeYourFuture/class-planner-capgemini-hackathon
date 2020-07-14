c743681c6e6aaf1bbe5e66d64eaaa4ad0bf16c7c
BANI GIT <bani.rezaie@gmail.com>
2020-07-14 19:38:12 +0100

    simple changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,9 @@
 import React, { useState, useEffect } from "react";
 import EachWeek from "./EachWeek";
-import Week from "./Week";
 import "./App.css";
 import "./Week.css";
-//import DetailsPage from "./DetailsPage";
 
-class App extends Component {
-
+const App = () => {
   const [weeks, setWeeks] = useState([]);
 
   useEffect(() => {
@@ -17,16 +14,14 @@ class App extends Component {
       });
   }, []);
 
-  render() {
-    return (
-      <div className="App-header">
-        <h1>Code Your Future Class Planner</h1>
-        <button className="edit-button">Edit Week/Add Week</button>
-        <EachWeek props={this.state.weeks}/>
-        <div className="holiday">
-          <p>Bank Holiday</p>
-          <p>23-Dec</p>
-        </div>
+  return (
+    <div className="App-header">
+      <h1>Code Your Future Class Planner</h1>
+      <button className="edit-button">Edit Week/Add Week</button>
+      <EachWeek props={weeks} />
+      <div className="holiday">
+        <p>Bank Holiday</p>
+        <p>23-Dec</p>
       </div>
     </div>
   );
