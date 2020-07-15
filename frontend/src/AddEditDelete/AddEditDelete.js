35464614177f5b2de216eb958cafdda188db81ee
mahmutsel <mahmutsel29@gmail.com>
2020-07-15 13:00:15 +0100

    navbar added

--- frontend/src/AddEditDelete/AddEditDelete.js
+++ frontend/src/AddEditDelete/AddEditDelete.js
@@ -2,12 +2,14 @@ import React from "react";
 import Add from "./Add";
 import EditDelete from "./EditDelete";
 import Find from "./Find";
+import Navbar from "../Navbar";
 const AddEditDelete = () => {
   return (
     <div>
+      <Navbar />
       <h1> Add/Edit/Delete Page</h1>
       <Add />
-      <Find/>
+      <Find />
       <EditDelete />
     </div>
   );
