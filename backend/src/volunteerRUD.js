dd9f75f71cd191b8401b81449923e1a825ae9699
hamidreza <hrfarzadeh@gmail.com>
2020-07-16 01:19:02 +0100

    put  funcionality finished

--- backend/src/volunteerRUD.js
+++ backend/src/volunteerRUD.js
@@ -2,28 +2,56 @@ import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
 
+
 const app = express();
 const weeks = require('../weeks.json')
 
+const bodyParser = require("body-parser");
+
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 
 app.get('/', (req, res) => {
   return res.send('Hello from the Class PLanner Backend!');
 });
 
-app.get('/volunteer', (req, res) => {
-  return res.send(Object.values(weeks));
-});
+// app.get('/volunteer', (req, res) => {
+//   return res.send(Object.values(weeks));
+// });
 
-app.get('/volunteer/:number', (req, res) => {
-  return res.send(weeks[req.params.number]);
-});
+// app.get('/volunteer/:number', (req, res) => {
+  
+// });
 
 
 app.put('/volunteer/:number', (req, res) => {
-  return res.send(`Received a PUT HTTP method for week number ${req.params.number}`);
-});
+  
+  const {fullName, email, role, slackId, comments} = req.query; 
+  
+  const weekSelected = weeks.find(element => element.week === Number(req.params.number))
+  
+  let volunteers = {}
+   
+  if(req.query.fullName || req.query.email || req.query.role || req.query.slackId ) {
+    volunteers = weekSelected.peopleDetails.find(person => person.fullName === fullName ||
+    person.email === email || person.role === role || person.slackId === slackId 
+  )}
+  if(req.body.FullName) {
+    volunteers.fullName = req.body.fullName
+  }
+  if(req.body.email) {
+    volunteers.email = req.body.email
+  }
+  if(req.body.role) {
+    volunteers.role = req.body.role
+  }
+  if(req.body.slackId) {
+    volunteers.slackId = req.body.slackId
+  }
+  res.json(volunteers);
+  })
 
 app.delete('/volunteer/:number', (req, res) => {
   return res.send(`Received a DELETE HTTP method for week number ${req.params.number}`);
