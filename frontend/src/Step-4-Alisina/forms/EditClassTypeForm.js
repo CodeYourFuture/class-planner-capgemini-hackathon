677556d0cddfd559dae4397b34611265a67b6043
[Alisina92] <asb92.jobs@gmail.com>
2020-07-17 09:54:03 +0100

    Step4-Add/Edit/Delete-Alisina completed

---
+++ frontend/src/Step-4-Alisina/forms/EditClassTypeForm.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+
+const EditClassTypeForm = (props) => {
+  const [user, setUser] = useState(props.currentUser);
+
+  useEffect(() => {
+    setUser(props.currentUser);
+  }, [props]);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        props.updateUser(user.id, user);
+      }}
+    >
+       
+      <label>ClassType</label>
+      <input
+        type="text"
+        name="classType"
+        value={user.classType}
+        onChange={handleInputChange}
+      />
+      <label>Start Time*:</label>
+      <input
+        type="time"
+        name="startTime"
+        value={user.startTime}
+        onChange={handleInputChange}
+      />
+      <label>FinishTime*:</label>
+      <input
+        type="time"
+        name="finishTime"
+        value={user.finishTime}
+        onChange={handleInputChange}
+      />
+      <button>Update user</button>
+      <button
+        onClick={() => props.setEditing(false)}
+        className="button muted-button"
+      >
+        Cancel
+      </button>
+    </form>
+  );
+};
+
+export default EditClassTypeForm;
---
+++ frontend/src/Step-4-Alisina/tables/ClassTable.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+const  ClassTable = (props) => (
+  <table>
+    <thead>
+      <tr>
+        <th>Class Type</th>
+        <th>Start Time</th>
+         <th>Finish Time</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      {props.users.length > 0 ? (
+        props.users.map((user) => (
+          <tr key={user.id}>
+            <td>{user.classType}</td>
+            <td>{user.startTime}</td>
+            <td>{user.finishTime}</td>
+            <td>
+              <button
+                className="button muted-button"
+                onClick={() => props.editRow(user)}
+              >
+                Edit
+              </button>
+              <button
+                className="button muted-button"
+                onClick={() => props.deleteUser(user.id)}
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+        ))
+      ) : (
+        <tr>
+          <td colSpan={3}>No classes has been added yet.</td>
+        </tr>
+      )}
+    </tbody>
+  </table>
+);
+
+export default ClassTable;
