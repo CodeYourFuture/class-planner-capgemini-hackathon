b4d0ceca7dee283d5c4cd9827b5308b9a6d90ff2
mahmutsel <mahmutsel29@gmail.com>
2020-07-21 15:08:33 +0100

    name changed

---
+++ frontend/src/TeacherDetails.js
@@ -0,0 +1,13 @@
+import React from "react";
+const TeacherDetails = () => {
+  return (
+    <ul className="list-group">
+      <li className="list-item">Coordinator </li>
+      <li className="list-item">Lead Teacher</li>
+      <li className="list-item">Assistant Lead Teacher</li>
+      <li className="list-item">Teaching Assistant</li>
+      <li className="list-item">Personal Development Rep</li>
+    </ul>
+  );
+};
+export default TeacherDetails;
